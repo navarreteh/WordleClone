4cf181910ff24da49ce4c12d0fd1a38018726855
Hannah Navarrete <26890727+navarreteh@users.noreply.github.com>
2022-08-02 20:14:41 -0400

    add flipTile function and style

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ const guessRows = [
 
 let currentRow = 0
 let currentTile = 0
+let isGameOver = false
 
 guessRows.forEach((guessRow, guessRowIndex) => {
     const rowElement = document.createElement('div')
@@ -110,6 +111,18 @@ const checkRow = () => {
         console.log('guess is ' + guess + ' wordle is ' + wordle)
         if(wordle == guess){
             showMessage('Magnificent!')
+            isGameOver = true
+            return
+        } else {
+            if(currentRow >= 5){
+                isGameOver = true
+                showMessage('Game Over')
+                return
+            }
+            if(currentRow < 5){
+                currentRow++
+                currentTile = 0
+            }
         }
     }
 }
@@ -119,4 +132,19 @@ const showMessage = (message) => {
     messageElement.textContent = message
     messageDisplay.append(messageElement)
     setTimeout(() => messageDisplay.removeChild(messageElement), 2000)
+}
+
+const flipTile = () => {
+    const rowTiles = document.querySelector('guessRow-' + currentRow).childNodes
+    rowTiles.forEach((tile, index) => {
+        const dataLetter = tile.getAttribute('data')
+
+        if(dataLetter == wordle[index]){
+            tile.classList.add('green-overlay')
+        } else if(wordle.includes(dataLetter)) {
+            tile.classList.add('yellow-overlay')
+        } else {
+            tile.classList.add('gray-overlay')
+        }
+    })
 }
